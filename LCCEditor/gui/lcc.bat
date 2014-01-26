2d29b207793e25cef33bff11351cbeb28648162d
don ebert <ebert.donald@epa.gov>
2014-01-25 22:06:57 -0800

    batch files to freeze the LCC Editor into a standalone exectable for deployment

--- LCCEditor/gui/lcc.bat
+++ LCCEditor/gui/lcc.bat
@@ -1,3 +1,3 @@
-cd c:\Projects\ATtILA2\src\LCCEditor\LCCEditor\gui 
+:: cd d:\ATtILA2\src\LCCEditor\LCCEditor\gui 
 
-cxfreeze __init__.py --target-dir dist --target-name LCCEditor.exe --base-name Win32GUI --include-modules atexit,PySide.QtNetwork --icon buddha.ico
+cxfreeze __init__.py -OO --target-dir dist --target-name LCCEditor.exe --base-name Win32GUI --include-modules atexit,PySide.QtNetwork
